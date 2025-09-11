e816a3ccd963dc73ea3ed8b81e6d06e80f603e09
Heidi-Astrup <eaa24hsas@students.eaaa.dk>
2025-09-11 12:51:04 +0200

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { Routes, Route } from "react-router";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
-import "./App.css";
+import Contact from "./pages/Contact";
+import NavBar from "./components/NavBar";
+
+import "./index.css";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-        </Routes>
-      </BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </>
   );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import { NavLink } from "react-router";
+
+export default function NavBar() {
+  return (
+    <nav>
+      <h1>HEIDI ASTRUP</h1>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/about">About</NavLink>
+      <NavLink to="/projects">Projects</NavLink>
+      <NavLink to="/contact">Contact</NavLink>
+    </nav>
+  );
+}
