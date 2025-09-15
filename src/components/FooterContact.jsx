cfc22c79aaed0fd7847c127eceaf80144ec43ced
Heidi-Astrup <eaa24hsas@students.eaaa.dk>
2025-09-15 20:32:10 +0200

    Add new SVG icons for social media and icons
    Footer updated

--- src/components/FooterContact.jsx
+++ src/components/FooterContact.jsx
@@ -1,18 +1,35 @@
+import facebook from "../images/SoMe/awsomeFacebook.svg";
+import linkedin from "../images/SoMe/awsomeLinkedin.svg";
+import github from "../images/SoMe/awsomeGithub.svg";
+import phone from "../images/SoMe/phone3.svg";
+import mail from "../images/SoMe/mail3.svg";
+
 export default function FooterContact() {
   return (
     <div className="FooterContact">
       <div className="kontaktDel">
         <h2>KONTAKT</h2>
-        <p>
-          E-mail: <a href="mailto:Heidi@kabelmail.dk">Heidi@kabelmail.dk</a>
-        </p>
-        <p>
-          Tlf: <a href="tel:+4522367207">22 36 72 07</a>
-        </p>
+
+        <a href="mailto:Heidi@kabelmail.dk" className="contai">
+          <img src={mail} alt="Telefon ikon" />
+          Heidi@kabelmail.dk
+        </a>
+
+        <a href="tel:+4522367207" className="contai">
+          <img src={phone} alt="Telefon ikon" />
+          22 36 72 07
+        </a>
+
         <h3>SOCIALS</h3>
-        <p>LinkedIn</p>
-        <p>GitHub</p>
-        <p>FaceBook</p>
+        <a href="https://www.linkedin.com/in/heidi-astrup-b2661a325/">
+          <img src={linkedin} alt="" />
+        </a>
+        <a href="https://github.com/Heidi-Astrup">
+          <img src={github} alt="" />
+        </a>
+        <a href="https://www.facebook.com/heidi.astrup.900">
+          <img src={facebook} alt="" />
+        </a>
       </div>
       <form
         action="mailto:Heidi@kabelmail.dk"
