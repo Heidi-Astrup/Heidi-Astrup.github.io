df1bf99e50eb42a6ca53129aad47b41f71578020
Heidi-Astrup <eaa24hsas@students.eaaa.dk>
2025-09-16 10:03:46 +0200

    Kontaktform done

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,47 @@
+import emailjs from "emailjs-com";
+
+export default function ContactForm() {
+  function sendEmail(e) {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_dci2fvu", // fra EmailJS
+        "template_4ga0l3n", // fra EmailJS
+        e.target,
+        "wBX-lCj07-vq_uE38" // fra EmailJS
+      )
+      .then(
+        (result) => {
+          console.log("Email sendt!", result.text);
+          alert("Tak! Din besked er sendt 🚀");
+        },
+        (error) => {
+          console.log("Fejl:", error.text);
+          alert("Ups, der skete en fejl 😢");
+        }
+      );
+
+    e.target.reset(); // ryd formularen
+  }
+
+  return (
+    <form on onSubmit={sendEmail}>
+      <label htmlFor="name">Navn</label>
+      <input id="name" type="text" placeholder="Navn" name="navn" />
+      <label htmlFor="emne">Emne</label>
+      <input id="emne" type="text" placeholder="Emne" name="emne" />
+      <label htmlFor="mail">Mail</label>
+      <input id="mail" type="email" placeholder="Email" name="email" />
+      <label htmlFor="besked">Skriv en besked</label>
+      <textarea
+        className="besked"
+        id="besked"
+        cols="30"
+        rows="3"
+        placeholder="Besked"
+        name="besked"
+      ></textarea>
+      <button type="submit">Send og få verdens bedste praktikant</button>
+    </form>
+  );
+}
--- src/components/FooterContact.jsx
+++ src/components/FooterContact.jsx
@@ -3,18 +3,41 @@ import linkedin from "../images/SoMe/awsomeLinkedin.svg";
 import github from "../images/SoMe/awsomeGithub.svg";
 import phone from "../images/SoMe/phone3.svg";
 import mail from "../images/SoMe/mail3.svg";
+import ContactForm from "../components/ContactForm";
+import emailjs from "emailjs-com";
 
 export default function FooterContact() {
+  function sendEmail(e) {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_dci2fvu", // fra EmailJS
+        "template_4ga0l3n", // fra EmailJS
+        e.target,
+        "wBX-lCj07-vq_uE38" // fra EmailJS
+      )
+      .then(
+        (result) => {
+          console.log("Email sendt!", result.text);
+          alert("Tak! Din besked er sendt 🚀");
+        },
+        (error) => {
+          console.log("Fejl:", error.text);
+          alert("Ups, der skete en fejl 😢");
+        }
+      );
+
+    e.target.reset(); // ryd formularen
+  }
+
   return (
     <div className="FooterContact">
       <div className="kontaktDel">
         <h2>KONTAKT</h2>
-
         <a href="mailto:Heidi@kabelmail.dk" className="contai">
           <img src={mail} alt="Telefon ikon" />
           Heidi@kabelmail.dk
         </a>
-
         <a href="tel:+4522367207" className="contai">
           <img src={phone} alt="Telefon ikon" />
           22 36 72 07
@@ -31,28 +54,7 @@ export default function FooterContact() {
           <img src={facebook} alt="" />
         </a>
       </div>
-      <form
-        action="mailto:Heidi@kabelmail.dk"
-        method="POST"
-        className="kontaktForm"
-      >
-        <label htmlFor="name">Navn</label>
-        <input id="name" type="text" placeholder="Navn" />
-        <label htmlFor="mail">Mail</label>
-        <input id="mail" type="email" placeholder="Email" />
-        <label htmlFor="emne">Emne</label>
-        <input id="emne" type="text" placeholder="Emne" />
-        <label htmlFor="besked">Skriv en besked</label>
-        <textarea
-          className="besked"
-          name=""
-          id="besked"
-          cols="30"
-          rows="3"
-          placeholder="Besked"
-        ></textarea>
-        <button type="submit">Send og få verdens bedste praktikant</button>
-      </form>
+      <ContactForm />
     </div>
   );
 }
