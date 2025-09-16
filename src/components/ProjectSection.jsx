e89dbeb34c4a44b85cbe04d10c89282d876610ab
Heidi-Astrup <eaa24hsas@students.eaaa.dk>
2025-09-16 13:30:22 +0200

    ny måde at vide data

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -1,40 +1,26 @@
-import { useState, useEffect } from "react";
 import { NavLink } from "react-router";
 
-export default function ProjectSection() {
-  const [projekter, setProjekter] = useState([]);
-
-  useEffect(() => {
-    async function fetchProjects() {
-      const response = await fetch("/data/projects.json");
-      const data = await response.json();
-      setProjekter(data);
-    }
-    fetchProjects();
-  }, []);
-
+export default function ProjectSection({ projekt }) {
   return (
     <section className="project-section">
-      {projekter.map((projekt) => (
-        <section key={projekt.id} className="projekt-card">
-          <article>
-            <h3>{projekt.title}</h3>
-            <h4>{projekt.subtitle}</h4>
-            <p>{projekt.description}</p>
-            <p>{projekt.year}</p>
-            <p>{projekt.tags.join(" | ")}</p>
-            <NavLink to={`/projects/${projekt.id}`}>
-              Se hele projektet her &#x2192;{" "}
-            </NavLink>
-            <a href={projekt.url}>
-              <button>Se websiden online</button>{" "}
-            </a>
-          </article>
-          <figure>
-            <img src={projekt.image} alt={projekt.title} />
-          </figure>
-        </section>
-      ))}
+      <section className="projekt-card">
+        <article>
+          <h3>{projekt.title}</h3>
+          <h4>{projekt.subtitle}</h4>
+          <p>{projekt.description}</p>
+          <p>{projekt.year}</p>
+          <p>{projekt.tags.join(" | ")}</p>
+          <NavLink to={`/projects/${projekt.id}`}>
+            Se hele projektet her &#x2192;{" "}
+          </NavLink>
+          <a href={projekt.url}>
+            <button>Se websiden online</button>{" "}
+          </a>
+        </article>
+        <figure>
+          <img src={projekt.image} alt={projekt.title} />
+        </figure>
+      </section>
     </section>
   );
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,24 @@
+import { useState, useEffect } from "react";
+
 import ProjectSection from "../components/ProjectSection";
 
 export default function Projects() {
+  const [projekter, setProjekter] = useState([]);
+
+  useEffect(() => {
+    async function fetchProjects() {
+      const response = await fetch("/data/projects.json");
+      const data = await response.json();
+      setProjekter(data);
+    }
+    fetchProjects();
+  }, []);
+
   return (
     <main className="Projects">
-      <ProjectSection />
+      {projekter.map((projekt) => (
+        <ProjectSection client={projekt} key={projekt.id} />
+      ))}
     </main>
   );
 }
