e6333ef92827037d44c3cf7607499db2ef333a0b
Heidi-Astrup <eaa24hsas@students.eaaa.dk>
2025-09-16 12:37:02 +0200

    responsiv navbar til mobil (burgermenu)

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,32 +1,51 @@
+import { useState } from "react";
 import { NavLink } from "react-router";
 
 export default function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav>
       <NavLink to="/" className="stor">
         <h1>HEIDI ASTRUP</h1>
       </NavLink>
-      <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>
-        Hjem
-      </NavLink>
-      <NavLink
-        to="/projects"
-        className={({ isActive }) => (isActive ? "active" : "")}
+
+      <div
+        className="menu"
+        onClick={() => {
+          setMenuOpen(!menuOpen);
+        }}
       >
-        Projekter
-      </NavLink>
-      <NavLink
-        to="/about"
-        className={({ isActive }) => (isActive ? "active" : "")}
-      >
-        Om mig
-      </NavLink>
-      <NavLink
-        to="/contact"
-        className={({ isActive }) => (isActive ? "active" : "")}
-      >
-        Kontakt
-      </NavLink>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <section className={menuOpen ? "open" : ""}>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Hjem
+        </NavLink>
+        <NavLink
+          to="/projects"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Projekter
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Om mig
+        </NavLink>
+        <NavLink
+          to="/contact"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Kontakt
+        </NavLink>
+      </section>
     </nav>
   );
 }
