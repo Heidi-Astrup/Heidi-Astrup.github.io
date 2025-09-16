2cba7cfb6dc2dfae09e18911d01ec364122eb558
Heidi-Astrup <eaa24hsas@students.eaaa.dk>
2025-09-16 20:14:51 +0200

    forbedret om mig tekst

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,22 +4,32 @@ export default function About() {
   return (
     <main className="About">
       <article className="abouttekst">
-        <h3>Heidi Astrup</h3>
+        <h3>Lidt om mig</h3>
         <p>
-          Multimediedesignstuderende med Speciale i <strong>frontend</strong>{" "}
-          udvikling
+          Hej, jeg hedder Heidi, og jeg studerer multimediedesign med speciale i{" "}
+          <strong>frontend-udvikling</strong>. Jeg har en stor passion for
+          programmering, som også er hovedårsagen til mit uddannelsesvalg – men
+          jeg nyder samtidig at udfolde mig kreativt.
         </p>
         <br />
         <p>
-          Udover studiet arbejder jeg som bordtennistræner, hvilket har givet
-          mig erfaring med at <strong>tage ansvar</strong>, at formidle viden og
-          skabe engagement hos andre – kompetencer jeg også tager med ind i mit
+          Når jeg arbejder, er jeg{" "}
+          <strong>systematisk, fokuseret og detaljeorienteret</strong>. Jeg
+          sørger altid for at gøre en opgave færdig og stræber ikke blot efter
+          at den er “fin nok”, men at den er{" "}
+          <strong>grundig og velovervejet</strong>.
+        </p>
+        <br />
+        <p>
+          Ved siden af studiet arbejder jeg som bordtennistræner. Her har jeg
+          fået erfaring med at <strong>tage ansvar</strong>, formidle viden og
+          skabe engagement – kompetencer, jeg også tager med mig ind i mit
           arbejde som multimediedesigner.
         </p>
         <br />
         <p>
-          Jeg elsker at lære nyt og håber på at kunne <strong>udvikle</strong>{" "}
-          mig meget under min praktik
+          Jeg elsker at lære nyt og ser frem til at <strong>udvikle</strong> mig
+          yderligere gennem min kommende praktik.
         </p>
         <br />
       </article>
